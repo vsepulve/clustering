8024b284607b75112472c0e2cc43bdeaec1c7b87
vsepulve <vsepulve@dcc.uchile.cl>
2019-04-24 18:12:41 -0400

    Code for Distance Distribution Analysis moved to DistancesDistributionAnalyzer

--- src/cl/citiaps/clustering/model/DistancesDistributionAnalyzer.java
+++ src/cl/citiaps/clustering/model/DistancesDistributionAnalyzer.java
@@ -3,14 +3,15 @@ package cl.citiaps.clustering.model;
 
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class ClusteringClarans implements Clustering {
+public class DistancesDistributionAnalyzer {
 	
 	private int sampling = 1000000;
 	
-	public ClusteringClarans(int _sampling){
+	public DistancesDistributionAnalyzer(int _sampling){
 		sampling = _sampling;
 	}
 	
@@ -19,19 +20,9 @@ public class ClusteringClarans implements Clustering {
 		Map<Integer, Integer> histo = new TreeMap<Integer, Integer>();
 		int n_buckets = 100;
 		
-		// Matriz Completa
-//		for(int i = 0; i < data.size(); ++i){
-//			for(int j = i+1; j < data.size(); ++j){
-//				Data d1 = data.get(i);
-//				Data d2 = data.get(j);
-//				double d = dist.compute(d1, d2);
-////				System.out.println("Dist " + d1.getId() + " X " + d2.getId() + " : " + String.format("%.2f", d));
-//				int pos = (int)(d*n_buckets);
-//				histo.putIfAbsent(pos, 0);
-//				histo.put(pos, histo.get(pos) + 1);
-//			}
-//		}
-		
+		List<Double> distancias = new LinkedList<Double>();
+		double min = 1000000;
+		double max = 0.0;
 		// Muestreo
 		for(int i = 0; i < sampling; ++i){
 			int pos_1 = ThreadLocalRandom.current().nextInt(0, data.size());
@@ -42,6 +33,26 @@ public class ClusteringClarans implements Clustering {
 			Data d1 = data.get(pos_1);
 			Data d2 = data.get(pos_2);
 			double d = dist.compute(d1, d2);
+			distancias.add(d);
+			if( d > max ){
+				max = d;
+			}
+			if( d < min ){
+				min = d;
+			}
+//			int pos = (int)(d*n_buckets);
+//			histo.putIfAbsent(pos, 0);
+//			histo.put(pos, histo.get(pos) + 1);
+		}
+		
+		// Normalizacion
+		System.out.println("Normalizando en [" + min + ", " + max + "]");
+		final double fixed_min = min;
+		final double fixed_max = max;
+		distancias.replaceAll( d -> (1.0 - (d-fixed_min)/(fixed_max-fixed_min)) );
+		
+		// Generacion del histograma
+		for( double d : distancias ){
 			int pos = (int)(d*n_buckets);
 			histo.putIfAbsent(pos, 0);
 			histo.put(pos, histo.get(pos) + 1);
